c3823d2591b65519854903c52e79d633fe799448
s-kvng <105977478+s-kvng@users.noreply.github.com>
2022-12-05 01:26:37 +0000

    Update on Email feature
    Email feature successfully integrated

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     #path('', views.post_list , name='post_list'), #function based view 
     path('', views.PostListView.as_view(), name='post_list'), #class based view , that uses the generic ListView
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_details , name='post_details'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail 
 
 
 # Create your views here.
@@ -35,7 +37,7 @@ def post_details(request,year,month,day,post):
     
     return render(request, 'blog/post/details.html', {'post': post})
 
-class PostListView(ListView):
+class PostListView(ListView): #class based view , inherited from ListView
     """
     Alternative to post_list view
     """
@@ -44,3 +46,37 @@ class PostListView(ListView):
     context_object_name= 'posts'
     paginate_by= 3
     template_name= 'blog/post/list.html'
+    
+    
+    
+def post_share(request, post_id):
+    
+    post = get_object_or_404(Post, id=post_id, status = Post.Status.PUBLISHED)
+    sent = False
+    
+    
+    if request.method == 'POST':
+        #form was submitted
+        
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form feilds passed validation
+            cd = form.cleaned_data # captures the inputed data in a dictionary form (fields as keys and input as values)
+            
+            #send mail
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            
+            subject = f"{cd['name']} recommended you read" \
+                        f' "{post.title}"'
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comment']}"
+                      
+            send_mail(subject, message , 'dproject8420@gmail.com', [cd['to']])
+            
+            sent=True
+            
+    else:
+        form = EmailPostForm()
+        
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form , 'sent': sent})
+    
