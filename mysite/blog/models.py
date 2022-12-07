b4cf21179e6ce10f1efd2c1493d51e3b9305d551
s-kvng <105977478+s-kvng@users.noreply.github.com>
2022-12-07 00:07:06 +0000

    Update on Comment Feature
    Almost done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -55,9 +55,10 @@ class Post(models.Model):
 
 class Comment(models.Model):
     
-    post = models.ForeignKey(Post, on_delete= models.CASCADE , related_name='comment')
+    post = models.ForeignKey(Post, on_delete= models.CASCADE , related_name='comments')
     name = models.CharField(max_length=80)
-    email = models.EmailField()
+    email = models.EmailField(max_length=100)
+    body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
